2ce6d70ad8b5976dba1dac223174b4858b288175
JJorgensen <jasoncjorgensen@gmail.com>
2021-05-05 20:13:50 -0600

    adding MERN stack

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Nav from "./components/Nav";
+import { BrowserRouter } from "react-router-dom";
+
+// The app will not render correctly until you setup a Route component.
+// Refer to the Basic Example documentation if you need to.
+// (https://reacttraining.com/react-router/web/example/basic)
+function App() {
+
+}
+
+export default App;
---
+++ scripts/seedDB.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/reactreadinglist"
+);
+
