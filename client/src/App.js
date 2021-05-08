5afcdae5e11bca28836bb05652090d37a0d3b60f
JJorgensen <jasoncjorgensen@gmail.com>
2021-05-08 11:00:30 -0600

    adding base html

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Landing from "./pages/Landing";
 
-import Nav from "./components/Nav";
+
+
 import { BrowserRouter } from "react-router-dom";
 
 // The app will not render correctly until you setup a Route component.
 // Refer to the Basic Example documentation if you need to.
 // (https://reacttraining.com/react-router/web/example/basic)
 function App() {
-
-}
+    return(
+    <div>
+        <Header />
+        <Landing />
+        <Footer />
+    </div>
+    )}
 
 export default App;
