6a4e91e4a277caf782d365b6b4a20a6fe38b2bc5
MattV <mvpetersen1@gmail.com>
2021-05-11 15:14:53 -0600

    added auth0 login

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
+import './styles/tailwind.css';
+import Auth0ProviderWithHistory from "./auth0-provider-with-history"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Auth0ProviderWithHistory>
+        <App/>
+
+    </Auth0ProviderWithHistory>
+    , document.getElementById("root")
+);
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from '../components/Header/Header'
+import Footer from "../components/Footer/Footer";
 
 export default function Login() {
     return (
         <div>
+            <Header></Header>
             <h1> Login</h1>
+            <Footer></Footer>
             
         </div>
     )
