bac966c0027304e756968a837f28c2546562fbea
MattV <mvpetersen1@gmail.com>
2021-05-19 21:19:30 -0600

    update data GET routes and logged with poppulate

---
+++ controllers/dataController.js
@@ -0,0 +1,30 @@
+const db = require("../models/");
+const dbUser = db.User;
+
+// Defining methods for the user Controller
+module.exports = {
+  findAll: function (req, res) {
+    console.log("findAll")
+    console.log(req)
+    dbUser
+      .find({email: req.query})
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    console.log("looking by ID")
+    console.log(req.params)
+    dbUser
+      .findById(req.params.id)
+      .populate([
+        {"path": "incomes"},
+        {"path": "debts"},
+        {"path": "exenses"},
+        {"path": "investments"}
+      ])
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  
+};
