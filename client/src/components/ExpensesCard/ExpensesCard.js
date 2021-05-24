1f60fe59f0b0a911b058f3492375da8718c78201
JJorgensen <jasoncjorgensen@gmail.com>
2021-05-23 23:56:08 -0600

    Income card working, Tax API working, Net income is being calculated and used in the 30 yr retirement account contribution

--- client/src/components/ExpensesCard/ExpensesCard.js
+++ client/src/components/ExpensesCard/ExpensesCard.js
@@ -10,28 +10,28 @@ export default function ExpensesCard(props) {
             <div className="z-0 grid grid-flow-row grid-cols-12 grid-rows gap-2 mt-3">
                 <div className="text-black font-bold text-xl mb-2">Expenses</div>
                 <div className="mb-5 col-start-1 col-span-6 my-2 w-full rounded">
-                    <p className="text-grey-darker text-base"><strong>Rent or Mortgage: </strong>{`$${props.expenses.["Rent or Mortgage"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Renters Insurance: </strong>{`$${props.expenses.["Renters Insurance"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Health Insurance: </strong>{`$${props.expenses.["Health Insurance"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Car Insurance: </strong>{`$${props.expenses.["Car Insurance"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Car Maintenance: </strong>{`$${props.expenses.["Car Maintenance"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Gas Expense: </strong>{`$${props.expenses.["Gas Expense"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Groceries: </strong>{`$${props.expenses.["Groceries"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Home Goods: </strong>{`$${props.expenses.["Home Goods"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Hygiene Products: </strong>{`$${props.expenses.["Hygiene Products"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Restaurants: </strong>{`$${props.expenses.["Restaurants"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Rent or Mortgage: </strong>{`$${props?.expenses?.["Rent or Mortgage"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Renters Insurance: </strong>{`$${props.expenses?.["Renters Insurance"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Health Insurance: </strong>{`$${props.expenses?.["Health Insurance"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Car Insurance: </strong>{`$${props.expenses?.["Car Insurance"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Car Maintenance: </strong>{`$${props.expenses?.["Car Maintenance"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Gas Expense: </strong>{`$${props.expenses?.["Gas Expense"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Groceries: </strong>{`$${props.expenses?.["Groceries"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Home Goods: </strong>{`$${props.expenses?.["Home Goods"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Hygiene Products: </strong>{`$${props.expenses?.["Hygiene Products"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Restaurants: </strong>{`$${props.expenses?.["Restaurants"]}`}</p>
                 </div>
 
                 <div className="mb-5 col-start-7 col-span-6 my-2 w-full rounded">
-                    <p className="text-grey-darker text-base"><strong>Phone Payment: </strong>{`$${props.expenses.["Phone Payment"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Internet Bill: </strong>{`$${props.expenses.["Internet Bill"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Water/Trash/Sewage: </strong>{`$${props.expenses.["Water/Trash/Sewage"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Electricity: </strong>{`$${props.expenses.["Electricity"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Natural Gas: </strong>{`$${props.expenses.["Natural Gas"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Clothing: </strong>{`$${props.expenses.["Clothing"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Haircuts: </strong>{`$${props.expenses.["Haircuts"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Self Care: </strong>{`$${props.expenses.["Self Care"]}`}</p>
-                    <p className="text-grey-darker text-base"><strong>Groceries: </strong>{`$${props.expenses.["Groceries"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Phone Payment: </strong>{`$${props.expenses?.["Phone Payment"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Internet Bill: </strong>{`$${props.expenses?.["Internet Bill"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Water/Trash/Sewage: </strong>{`$${props.expenses?.["Water/Trash/Sewage"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Electricity: </strong>{`$${props.expenses?.["Electricity"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Natural Gas: </strong>{`$${props.expenses?.["Natural Gas"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Clothing: </strong>{`$${props.expenses?.["Clothing"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Haircuts: </strong>{`$${props.expenses?.["Haircuts"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Self Care: </strong>{`$${props.expenses?.["Self Care"]}`}</p>
+                    <p className="text-grey-darker text-base"><strong>Groceries: </strong>{`$${props.expenses?.["Groceries"]}`}</p>
                 </div>
             </div>
         </div>
---
+++ client/src/components/IncomeCard/IncomeCard.js
@@ -0,0 +1,21 @@
+import React, {useEffect} from 'react'
+
+function IncomeCard({incomeData,totalTax,investmentsData}) {
+    const netIncome = incomeData?.["Gross Income"] +incomeData?.["Other Additional Income"]- totalTax?.state?.amount - totalTax?.federal?.amount - totalTax?.fica?.amount;
+
+
+    return (
+        <div className="mb-8">
+        <div className="text-black font-bold text-xl mb-2">Financial Snap Shot</div>
+        <p className="text-grey-darker text-base"><strong>Gross Income: </strong>${incomeData?.['Gross Income']}</p>
+        <p className="text-grey-darker text-base"><strong>Additional Income: </strong>${incomeData?.['Other Additional Income']}</p>
+        <p className="text-grey-darker text-base"><strong>Estimated Federal Tax: </strong>${totalTax?.federal?.amount}</p>
+        <p className="text-grey-darker text-base"><strong>Estimated State Tax: </strong>${totalTax?.state?.amount}</p>
+        <p className="text-grey-darker text-base"><strong>Estimated FICA Tax: </strong>${totalTax?.fica?.amount}</p>
+        <p className="text-grey-darker text-base"><strong>After Tax Income: </strong>${netIncome}</p>  
+        <p className="text-grey-darker text-base"><strong>Current Retirement Balance </strong>${investmentsData?.['Current Retirement Balance']}</p>
+        </div>
+    )
+}
+
+export default IncomeCard
