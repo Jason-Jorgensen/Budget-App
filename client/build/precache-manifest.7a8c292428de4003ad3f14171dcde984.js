39544a21cfffabdabfaf2e7d442fe9d7df3d1945
JJorgensen <jasoncjorgensen@gmail.com>
2021-05-24 22:29:53 -0600

    build

---
+++ client/build/precache-manifest.7a8c292428de4003ad3f14171dcde984.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1641adcf9fa8093e3beb1511e965b85f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd8f8470ed699c30974d",
+    "url": "/static/css/2.a9b8d2fa.chunk.css"
+  },
+  {
+    "revision": "31bf1c99aa616c94a347",
+    "url": "/static/css/main.dfecf4a4.chunk.css"
+  },
+  {
+    "revision": "bd8f8470ed699c30974d",
+    "url": "/static/js/2.b8725656.chunk.js"
+  },
+  {
+    "revision": "14a2499832bc02ee92584e1d46e4e1e2",
+    "url": "/static/js/2.b8725656.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bf1c99aa616c94a347",
+    "url": "/static/js/main.a02af45c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "be84535235c6dfc17be75cd48dfb3277",
+    "url": "/static/media/pexels-karolina-grabowska-4386476.be845352.jpg"
+  }
+]);
